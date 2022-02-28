635e30098d45f98aef9b9fab96cc3769dfa397bb
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-02-28 16:54:11 +0300

    fixed syntax

--- test_app.py
+++ test_app.py
@@ -7,11 +7,12 @@ def test_not_get_lessons():
 
 
 def test_get_lessons():
-    assert timetable.get_lessons_for_day(date(2022, 2, 25)) == [{'name': 'ФМ и ИА', 'time': '9:45', 'group': None, 'online': False},
-                                                                 {'name': 'АБД', 'time': '13:25', 'group': None, 'online': False},
-                                                                 {'name': 'ИС УПК', 'time': '15:10', 'group': None, 'online': False},
-                                                                 {'name': 'РИКТ и ОП', 'time': '16:55', 'group': None, 'online': False},
-                                                                 {'name': 'ИС УПК', 'time': '18:40', 'group': 2, 'online': False}], "Should be a list of dicts"
+    assert timetable.get_lessons_for_day(date(2022, 2, 25)) == \
+           [{'name': 'ФМ и ИА', 'time': '9:45', 'group': None, 'online': False},
+            {'name': 'АБД', 'time': '13:25', 'group': None, 'online': False},
+            {'name': 'ИС УПК', 'time': '15:10', 'group': None, 'online': False},
+            {'name': 'РИКТ и ОП', 'time': '16:55', 'group': None, 'online': False},
+            {'name': 'ИС УПК', 'time': '18:40', 'group': 2, 'online': False}], "Should be a list of dicts"
 
 
 

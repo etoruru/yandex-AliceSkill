68aedabbaf31793f0c91ee5221d30c9e28c2998a
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-03-01 15:26:40 +0300

    added tests for making phrase on today

--- request.py
+++
@@ -1,6 +0,0 @@
-class Request:
-    def __init__(self, request_body):
-        self.request_body = request_body
-
-    def __getitem__(self, key):
-        return self.request_body[key]
--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
-import timetable
 from datetime import date
+import timetable
+import main
+import time_machine
 
 
 def test_not_get_lessons():
@@ -13,6 +15,25 @@ def test_get_lessons():
             {'name': 'ИС УПК', 'time': '15:10', 'group': None, 'online': False},
             {'name': 'РИКТ и ОП', 'time': '16:55', 'group': None, 'online': False},
             {'name': 'ИС УПК', 'time': '18:40', 'group': 2, 'online': False}], "Should be a list of dicts"
+
+
+def test_query_lessons_for_today():
+    assert main.is_query_timetable_today('какие сегодня уроки') == True
+
+
+def test_query_lessons_for_not_today():
+    assert main.is_query_timetable_today('какие завтра пары') == False
+
+
+def test_make_phrase_not_have_lessons_today():
+    assert main.make_today_lessons_phrase() == 'Сегодня нет пар', "Should be phrase 'no lessons today' "
+
+
+def test_make_phrase_have_lessons_today():
+    with time_machine.travel(date(2022, 2, 25)):
+        assert main.make_today_lessons_phrase() == 'Сегодня у вас: ФМ и ИА, АБД, ИС УПК, РИКТ и ОП, ИС УПК'
+
+
 
 
 

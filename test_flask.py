2852a60433692e1fb0427ad773509d0d094a7ca8
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-03-27 14:01:19 +0300

    added response for thank phrase and tests for it

--- test_flask.py
+++ test_flask.py
@@ -62,3 +62,12 @@ def test_main_app_tomorrow(client):
          })
          assert response.json["response"]["text"].lstrip('Завтра нет пар, но помните ') in sayings.HARM_IDLENESS
 
+
+def _test_main_app_thank(client):
+    response = client.post('/alice', json={
+        "request": {
+            "command": "Cпасибо большое"
+        }
+    })
+    assert response.json["response"]["text"].lstrip('Пожалуйста, и помните ') in sayings.THANK_RESPONSE
+
